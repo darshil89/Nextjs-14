1b60de3558334e6a158d1849294a3a7851d25579
Darshil Mahraur <darshilmahraur67@gmail.com>
2023-11-25 21:58:57 +0530

    links done

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -3,8 +3,6 @@ import { useRouter } from "next/navigation";
 export default function ProfileID({ params }: { params: { id: string } }) {
   const router = useRouter();
 
-  console.log(params.id);
-
   return (
     <main className="container p-24">
       <h1 className="text-2xl">Profile page id - {params.id}</h1>
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function Profile() {
@@ -7,11 +8,19 @@ export default function Profile() {
     <main className="container min-h-screen  p-24">
       <h1 className="text-2xl">My list of profiles</h1>
 
-      <ul>
-        <li onClick={() => router.push("/profile/1")}>Profile 1</li>
-        <li onClick={() => router.push("/profile/2")}>Profile 2</li>
-        <li onClick={() => router.push("/profile/3")}>Profile 3</li>
-      </ul>
+      <div className="flex flex-col">
+        <Link href="/profile/1">Profile 1</Link>
+        <Link href="/profile/2">Profile 2</Link>
+        <Link
+          prefetch={true}
+          href={{
+            pathname: "/profile/1",
+            query: { userName: "Darshil", age: 12 },
+          }}
+        >
+          Profile 3
+        </Link>
+      </div>
     </main>
   );
 }
