34f33fc68b6ee8260392ea082802c7aa6cf47965
Darshil Mahraur <darshilmahraur67@gmail.com>
2023-11-27 20:53:38 +0530

    done with components

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Logo from "./Logo";
+export default function Header() {
+  return (
+    <div className="flex flex-row">
+      <div className="text-3xl mr-2">Header</div>
+      <Logo />
+
+    </div>
+  );
+}
---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Logo() {
+  return <div className="text-3xl">LOGO</div>;
+}
+
+
+
+
